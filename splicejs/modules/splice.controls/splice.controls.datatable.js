9185bdf9e4d911094e6ef17436ee6c39b9f33398
unknown <dmitry@unioninformatics.com>
2015-07-14 20:00:11 -0400

    Demo Controls mods. Included font type called "Roboto"

--- splicejs/modules/splice.controls/splice.controls.datatable.js
+++ splicejs/modules/splice.controls/splice.controls.datatable.js
@@ -146,20 +146,6 @@ definition:function(){
 	DataTable.prototype.addRowTo = function(destination, row){
 		
 		var newrow =  destination.insertRow();
-		
-		_.Event.create(newrow,'onmouseover').subscribe(function(){
-			for(var i=0; i<this.childNodes.length; i++){
-				this.childNodes[i].style.backgroundColor = "rgb(32, 58, 86)";
-			}
-		},newrow);
-
-		_.Event.create(newrow,'onmouseout').subscribe(function(){
-			for(var i=0; i<this.childNodes.length; i++){
-				this.childNodes[i].style.backgroundColor = "rgb(28,50,73)";
-			}
-		},newrow);
-
-
 
 		for(var i=0; i < row.length; i++ ){
 			var cell = newrow.insertCell();
