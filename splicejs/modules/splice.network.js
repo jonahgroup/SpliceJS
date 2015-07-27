eea057e131aa80aa48494d3ad3cbe15decd3d325
unknown <dmitry@unioninformatics.com>
2015-07-27 03:34:13 -0400

    Namespaces for remote calls

--- splicejs/modules/splice.network.js
+++ splicejs/modules/splice.network.js
@@ -10,16 +10,35 @@
 		if(!(calls instanceof Array)) calls = [calls];
 			
 		for(var i=0; i<calls.length; i++) {
-			var call = calls[i];
-			if(_.Remote[call] != null && _.Remote[call] != undefined ) continue;
-			_.Remote[call] = (function(){
+
+		    var call = calls[i]
+		    ,   nparts = call.split('.')
+            ,   remote = _.Remote
+            ,   fullCall = call;
+
+
+		    for (var n = 0; n < nparts.length-1; n++) {
+		        if (!remote[nparts[n]]) remote[nparts[n]] = {};
+		        remote = remote[nparts[n]];
+		    }
+
+		    call = nparts[nparts.length - 1];
+
+			if(remote[call] != null && remote[call] != undefined ) continue;
+
+			remote[call] = (function () {
 				var methodName = this.methodName;
 				var args = arguments;
+
 				return function(oncomplete, onfailure){
 					if(!oncomplete || typeof(oncomplete) !== 'function') return;
+
+					if (!SPLICE_REMOTE_ENDPOINT) //perform a stronger check for a valid URL syntax
+					    throw 'Remote end-point is not configured';
+
 					_.HttpRequest.post({
 						/* TODO: server end point URL, turn into a configurable property */
-						url:'remote',
+					    url: SPLICE_REMOTE_ENDPOINT,
 						
 						data:[{name:'rpc', value:_.ConstructCall(methodName,args)}],
 						
@@ -33,13 +52,13 @@
 						}
 					});
 				}
-			}).bind({methodName:call});
+			}).bind({methodName:fullCall});
 		}
 	};
 	
 	_.ConstructCall = function(methodName, args){
 		
-		jsonCall = '{"'+methodName+'":'+ JSON.stringify(args) + '}';
+		jsonCall = '{"call":"'+methodName+'","parameters":'+ JSON.stringify(args) + '}';
 		_.debug.log(jsonCall);
 		
 		return jsonCall;
